0cc8b8e0467b02f0507a4052d83b03c459043730
taofikmmd <taofikdimas@gmail.com>
2024-02-01 18:46:36 +0700

    Praktikum PBO - Java - commit kedua kali

---
+++ src/main/java/com/mycompany/ujicoba/Ujicoba.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ujicoba;
+
+/**
+ *
+ * @author Dimas
+ */
+public class Ujicoba {
+
+    public static void main(String[] args) {
+        String nim = "238512804";
+        String nama = "Taofik";
+        int angkatan = 2023;
+        double ipk = 4.00;
+        System.out.println("Hello World!");
+        
+        final String PRODI = "Teknik Informatika";
+        
+        System.out.println("NIM : "+nim);
+        System.out.println("Nama : "+nama);
+        System.out.println("Angkatan : "+angkatan);
+        System.out.println("Ipk : "+ipk);
+        System.out.println("Prodi : "+PRODI);
+    }
+}
